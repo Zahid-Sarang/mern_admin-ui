136bb3488c1c0fe11d14eab8d05272af4cb8efea
zahid-soft <zahidsarang95@gmail.com>
2023-12-24 23:23:42 +0530

    add usePermission hook

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -13,8 +13,9 @@ import { LockFilled, UserOutlined, LockOutlined } from "@ant-design/icons";
 import Logo from "../../components/icons/Logo";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { Credentials } from "../../types";
-import { login, self } from "../../http/api";
+import { login, self, logout } from "../../http/api";
 import { useAuthStore } from "../../store";
+import { usePermission } from "../../hooks/usePermission";
 
 const loginUser = async (credentials: Credentials) => {
 	// server call logic
@@ -28,8 +29,9 @@ const getSelf = async () => {
 };
 
 const LoginPage = () => {
-	const { setUser } = useAuthStore();
-	const { data: selfData, refetch } = useQuery({
+	const { isAllowed } = usePermission();
+	const { setUser, logout: logoutFromStore } = useAuthStore();
+	const { refetch } = useQuery({
 		queryKey: ["self"],
 		queryFn: getSelf,
 		enabled: false,
@@ -39,6 +41,14 @@ const LoginPage = () => {
 		mutationFn: loginUser,
 		onSuccess: async () => {
 			const selfDataPromise = await refetch();
+			// logout or redirect to client ui
+			// window.location.href = "http://clientui/url"
+			if (!isAllowed(selfDataPromise.data)) {
+				await logout();
+				logoutFromStore();
+				return;
+			}
+
 			setUser(selfDataPromise.data);
 		},
 	});

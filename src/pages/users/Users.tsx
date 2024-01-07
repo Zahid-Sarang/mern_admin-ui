2b1e2a8b4fdef7db24612407ce35cf0abdfc475f
zahid-soft <zahidsarang95@gmail.com>
2024-01-07 13:44:12 +0530

    add bread crumb

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,23 @@
+import { Breadcrumb } from "antd";
+import { RightOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+
 const Users = () => {
-	return <div>Users</div>;
+	return (
+		<>
+			<Breadcrumb
+				separator={<RightOutlined />}
+				items={[
+					{
+						title: <Link to="/">Dashboard</Link>,
+					},
+					{
+						title: "Users",
+					},
+				]}
+			/>
+		</>
+	);
 };
 
 export default Users;
